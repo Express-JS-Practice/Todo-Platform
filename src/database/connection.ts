a20f70726b5933b343d52ae2274bc00f197dade2
jhosepmyr <jhosepmyrgutierrezsoto@gmail.com>
2024-12-03 17:55:11 -0500

    feat: set connection string to real database

--- src/database/connection.ts
+++ src/database/connection.ts
@@ -1,12 +1,12 @@
 import { Sequelize } from "sequelize";
 
 const sequelize = new Sequelize({
-    host: 'localhost',
+    host: 'junction.proxy.rlwy.net',
     username: 'root',
-    password: 'jhosepmyr10',
-    database: 'shoppc',
+    password: 'iSQQhWkSiLgCiQnNQOeucAWeYjTjbBlQ',
+    database: 'taskwave',
     dialect: 'mysql',
-    port: 3306
+    port: 38930
 });
 
 export default sequelize
